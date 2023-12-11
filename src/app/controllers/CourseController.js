a59ee1e6dd42e4af388281cf168697659f0e7c7e
Nguyễn Hữu Duy Kha <94631848+dkhak3@users.noreply.github.com>
2023-12-11 23:13:14 +0700

    [CRUD] Create new course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -4,11 +4,25 @@ const { mongooseToObject } = require("../../util/mongoose");
 class CourseController {
   // [GET] /courses/:slug
   show(req, res, next) {
-    // res.send(req.params.slug);
     Course.findOne({ slug: req.params.slug })
       .then((course) => {
         res.render("courses/show", { course: mongooseToObject(course) });
       })
+      .catch(next);
+  }
+
+  // [GET] /courses/create
+  create(req, res, next) {
+    res.render("courses/create");
+  }
+
+  // [POST] /courses/store
+  store(req, res, next) {
+    req.body.image = `https://i.ytimg.com/vi/${req.body.videoId}/hq720.jpg`;
+    const course = new Course(req.body);
+    course
+      .save()
+      .then(() => res.redirect("/"))
       .catch(next);
   }
 }
