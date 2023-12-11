d8608d39facf6a7719a18164ca087847cb3d810a
Nguyễn Hữu Duy Kha <94631848+dkhak3@users.noreply.github.com>
2023-12-11 10:22:41 +0700

    [MVC] Routes & Controllers

--- src/index.js
+++ src/index.js
@@ -5,24 +5,30 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+const route = require("./routes");
+
 // đường dẫn ảnh http://localhost:3000/img/logo.png
 app.use(express.static(path.join(__dirname, "public")));
 
+// lấy giá trị của form method POST trong Form Data
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
 // HTTP Logger
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 
 // Template engine
 app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+// Route init
+route(app);
 
-app.get("/news", (req, res) => {
-  res.render("news");
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
