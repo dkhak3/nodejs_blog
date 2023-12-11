520116ee3fccacc55ed81974f2d3899da8a3cc98
Nguyễn Hữu Duy Kha <94631848+dkhak3@users.noreply.github.com>
2023-12-11 14:57:45 +0700

    [MVC] Model

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ const app = express();
 const port = 3000;
 
 const route = require("./routes");
+// import db
+const db = require("./config/db");
+
+// Connect to db
+db.connect();
 
 // đường dẫn ảnh http://localhost:3000/img/logo.png
 app.use(express.static(path.join(__dirname, "public")));
@@ -28,7 +33,6 @@ app.set("views", path.join(__dirname, "resources/views"));
 
 // Route init
 route(app);
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
