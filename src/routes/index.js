89768f0f342c40d9fd38961e00accda5a4f74ee5
Nguyễn Hữu Duy Kha <94631848+dkhak3@users.noreply.github.com>
2023-12-11 21:06:33 +0700

    Course detail page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const newsRouter = require("./news");
 const siteRouter = require("./site");
+const coursesRoute = require("./courses");
 
 function route(app) {
+  app.use("/courses", coursesRoute);
   app.use("/news", newsRouter);
   app.use("/", siteRouter);
 }
