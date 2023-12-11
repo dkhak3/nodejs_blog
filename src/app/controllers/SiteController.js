643c46bfd90d10447581e0b90d5a33d73fc797d9
Nguyễn Hữu Duy Kha <94631848+dkhak3@users.noreply.github.com>
2023-12-11 19:48:48 +0700

    [CRUD] Read from DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,13 +1,13 @@
 const Course = require("../models/Course");
+const { mutipleMongooseToObject } = require("../../util/mongoose");
 class SiteController {
   // [GET] /
-  async index(req, res) {
-    try {
-      const courses = await Course.find({});
-      res.json(courses);
-    } catch (error) {
-      res.status(400).json({ err: "ERROR!!!" });
-    }
+  index(req, res, next) {
+    Course.find({})
+      .then((courses) => {
+        res.render("home", { courses: mutipleMongooseToObject(courses) });
+      })
+      .catch(next);
 
     // res.render("home");
   }
--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ app.use(express.json());
 // Template engine
 app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
-app.set("views", path.join(__dirname, "resources/views"));
+app.set("views", path.join(__dirname, "resources", "views"));
 
 // Route init
 route(app);
